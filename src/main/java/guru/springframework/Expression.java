4c3d5a609b86e3447c399e7a7bd9e9ffe942f2e0
Ádám <suto.adam93@gmail.com>
2020-04-17 21:54:13 +0200

    Add testSumPlusMoney and testSumTimes

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -3,5 +3,7 @@ package guru.springframework;
 public interface Expression {
     Money reduce(Bank bank, String to);
 
+    Expression times(int multiplier);
+
     public Expression plus(Expression addend);
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -40,6 +40,7 @@ public class Money implements Expression {
                 '}';
     }
 
+    @Override
     public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
