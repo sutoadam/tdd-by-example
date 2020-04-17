ef4a88dd409bb18045e82fe964dff84c8a114658
Ádám <suto.adam93@gmail.com>
2020-04-17 20:58:53 +0200

    Add Sum Expression

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -28,6 +28,11 @@ public class Money implements Expression {
     }
 
     @Override
+    public Money reduce(String to) {
+        return this;
+    }
+
+    @Override
     public String toString() {
         return "Money{" +
                 "amount=" + amount +
@@ -40,6 +45,6 @@ public class Money implements Expression {
     }
 
     public Expression plus(Money addend){
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
 }
