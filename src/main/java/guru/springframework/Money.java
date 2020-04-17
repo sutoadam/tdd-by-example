036e3644fab98ef1cd3589b3649b186c70a3bcb7
Ádám <suto.adam93@gmail.com>
2020-04-17 21:41:05 +0200

    Add reduce mixed currency and start refactor

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -29,8 +29,6 @@ public class Money implements Expression {
 
     @Override
     public Money reduce(Bank bank, String to) {
-//        return this;
-        //int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
         return new Money(amount / bank.rate(this.currency, to), to);
     }
 
@@ -42,11 +40,12 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
 
-    public Expression plus(Money addend){
+    @Override
+    public Expression plus(Expression addend){
         return new Sum(this, addend);
     }
 }
